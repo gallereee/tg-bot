83040e968a5185a20ad4ee19b060287eaa804fd4
Dmitry Bardyshev <brd.dmitry@gmail.com>
2022-04-04 19:57:48 +0300

    Changed eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,9 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+	extends: ["@bd-dm/eslint-config-node"],
+	rules: {
+		"no-empty-function": "off",
+		"no-useless-constructor": "off",
+		"class-methods-use-this": "off",
+		"max-classes-per-file": "off",
+	},
 };
